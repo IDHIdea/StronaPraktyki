5de3b4837c8bf0548afef064d667b2d8bbd332a0
IDHIdea <reflextopyt@gmail.com>
2022-11-30 02:02:26 +0100

    dgdsgsdgdgs

--- przycisk.js
+++ przycisk.js
@@ -24,3 +24,32 @@ getFile("x.csv", function(csv) {
 	const lines = csv.split('\n') || csv.split('\r')
 	getFile("przycisk.html", function(btn) { createButtons(btn, lines) })
 })
+
+// slider
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("Slider");
+  let dots = document.getElementsByClassName("kropka");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
